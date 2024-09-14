f1b299d2c1dd99c0bcb6881725e18e742e91083f
Yazid Fauzan Prasatria <158979156+yuzid@users.noreply.github.com>
2024-09-14 07:44:10 +0700

    +

--- index.js
+++ index.js
@@ -3,16 +3,33 @@ const QRCode = require('qrcode');
 const app = express();
 const PORT = 3000;
 
+var opts = {
+  errorCorrectionLevel: 'H',
+  type: 'image/jpeg',
+  quality: 0.3,
+  margin: 1,
+  color: {
+    dark:"#FF7F0000",
+    light:"#FFBF60FF"
+  }
+}
+
 app.get('/generateQR', async (req, res) => {
   try {
-    const url = req.query.url || 'abcd';
-    const qrCodeImage = await QRCode.toDataURL(url);
+    const url = req.query.url || 'aiuerde';
+    const qrCodeImage = await QRCode.toDataURL(url,opts);
     res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
-  } catch (err) {
+    // res.send(200, {
+    //   id: qrCodeImage
+    // }) 
+  } 
+  catch (err) {
     console.error('Error generating QR code:', err);
     res.status(500).send('Internal Server Error');
   }
+
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
