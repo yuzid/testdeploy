ec9b185eaebf0293d5fceb91840e9ee0c5f4bbf5
Muhammad Reivan Naufal Mufid <158981374+ReivnNaufl@users.noreply.github.com>
2024-09-18 13:45:37 +0700

    Update fungsi redirect dan buat Add shortlink
    Buat fungsi redirect membaca dari database dan menambahkan fungsi untuk menyimpan data shortlink dari user dan menyimpannya ke database

--- src/index.js
+++ src/index.js
@@ -1,16 +1,124 @@
-import express from 'express';
+import express, { json } from 'express';
 const app = express();
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 import cryptoRandomString from 'crypto-random-string';
+import pg from 'pg';
 const PORT = 8000;
 
-app.use(express.json())
+const pool = new pg.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'SL',
+    password: 'NeaRzro2206!',
+    port: 5432,
+  });
+  
+
+app.use(json())
 
 app.listen(PORT, ()=>
     console.log("Running at ", {PORT})
 )
 
-app.get('/:id', (req, res) => {
+
+app.post('/a/activation', async (req, res) =>{
+    const {body} = req;
+    let hashed = await bcrypt.hash(body.password, 11);
+    pool.query(`INSERT INTO account (email, password) values ('${body.email}', '${hashed}')`);
+    res.status(200).send({
+        msg: 'berhasil'
+    });
+})
+
+app.get('/login', async (req,res) => {
+    const {body} = req;
+    const client = pool.connect();
+    let result = await (await client).query(`SELECT password FROM account WHERE email = '${body.email}'`);
+    (await client).end();
+    console.log(await bcrypt.compare(body.password, result.rows[0]['password']));
+    console.log(result.rows[0]['password'])
+    if (await bcrypt.compare(body.password, result.rows[0]['password'])){
+        res.send(200, {
+            msg: "Login berhasil"
+        });
+    }
+    else{
+        res.send(200, {
+            msg: "Login gagal"
+        });
+    }
+})
+
+app.get('/:id', async (req,res) => {
+    try{
+        const result = await pool.query(`SELECT id_shortlink FROM shortlink WHERE short_url = $1`, [req.params.id]);
+        res.redirect(301, `http://localhost:8080/sl/${result.rows[0]['id_shortlink']}`);
+    }
+    catch(err){
+        res.status(500).send('Server error')
+    }
+})
+
+app.post('/create/shortlink', async (req,res) => {
+    try{
+        const {body} =  req;
+        if (! await isCustomUnique(body.custom)){
+            res.status(404).send({
+                msg: "custom sudah ada"
+            });
+        }
+        else{
+            const id = await uniqueRandomID();
+            await pool.query(`INSERT INTO shortlink (id_shortlink, long_url, short_url, email, time_created) VALUES ($1, $2, $3, $4, now()::timestamp)`, [id, body.destination, body.custom, body.email]);
+            res.status(200).send({
+                msg:"selesai"
+            })
+        }
+    }
+    catch(err){
+        res.status(500).send('Server error')
+    }
+});
+
+async function isIDunique(id){
+    const result = await pool.query(`SELECT EXISTS(SELECT 1 FROM shortlink WHERE id_shortlink = $1)`, [id]);
+    return !result.rows[0]['exists'];
+}
+
+async function isCustomUnique(custom){
+    const result = await pool.query(`SELECT EXISTS(SELECT 1 FROM shortlink WHERE short_url = $1)`, [custom]);
+    return !result.rows[0]['exists'];
+}
+
+async function uniqueRandomID(){
+    let id;
+    while (true){
+        id = cryptoRandomString({length: 4, type:'alphanumeric'});
+        if (await isIDunique(id)){
+            break;
+        }
+    }
+    return id;
+}
+
+//test function
+
+app.get('/t/:id', async (req,res) => {
+    const result = await isIDunique(req.params.id);
+    console.log(result)
+    if (!result){
+        res.status(200).send({
+            msg:"Tidak Ada"
+        })
+    }
+    else{
+        res.status(404).send({
+            msg: "Ada"
+        })
+    }
+})
+
+app.get('/redirect/:id', (req, res) => {
     const {body} = req
     
     res.redirect(301, body.url)
@@ -47,20 +155,4 @@ app.get('/hash/time/test', async (req, res) => {
         sum: timeSum,
         avg: timeAvg
     })
-})
-
-app.get('/login/test', async (req,res) => {
-    const {body} = req
-    let hashed = await bcrypt.hash("Password123", 10) //ganti dengan ambil hash dari db
-    
-    if (await bcrypt.compare(body.password, hashed)){
-        res.send(200, {
-            msg: "Login berhasil"
-        })
-    }
-    else{
-        res.send(200, {
-            msg: "Login gagal"
-        })
-    }
 })
--- src/web2/index.js
+++ src/web2/index.js
@@ -1,10 +1,30 @@
 import express from 'express';
 const app = express();
+import pg from 'pg';
 const PORT = 8080;
+
+const pool = new pg.Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'SL',
+    password: 'NeaRzro2206!',
+    port: 5432,
+  });
+
 
 app.listen(PORT, ()=>
     console.log("Running at ", {PORT})
 )
+
+app.get('/sl/:id', async (req,res) => {
+    try{
+        const result = await pool.query(`SELECT long_url FROM shortlink WHERE id_shortlink = $1`, [req.params.id]);
+        res.redirect(301, result.rows[0]['long_url']);
+    }
+    catch(err){
+        res.status(500).send('Server error')
+    }
+})
 
 app.get("/:id", (req, res) => {
     let url = "https://youtube.com/watch?v="
