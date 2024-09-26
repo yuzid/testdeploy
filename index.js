69fea036da16dfc56bb6a42155e3737323b7d834
Muhammad Reivan N M <reivnnaufl@gmail.com>
2024-09-26 15:23:40 +0700

    Send html example

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import shortlinkController from "./src/controllers/shortlinkController.js";
 import routerShortlink from './src/routes/shortlink.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const PORT = 8000;
 const app = express();
 
@@ -8,12 +13,14 @@ app.use(express.json());
 
 app.use('/shortlink', routerShortlink);
 
+app.use('/assets', express.static(path.join(__dirname, 'src', 'views', 'assets')));
+
 app.listen(PORT, () => {
     console.log(`Server utama running at port ${PORT}`);
 });
 
-app.get('/', () => {
-    //render smthng
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src', 'views', 'index.html'));
 })
 
 app.get('/:id', shortlinkController.firstRedirect);
