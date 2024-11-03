8d022d9fb02a16a294fa740c51c54dd98601bc50
yuzid <yazid.fauzan9999@gmail.com>
2024-11-03 17:04:36 +0700

    Perubahan penyimpanan qr menjadi Logo agar customizable

--- src/models/qrModel.js
+++ src/models/qrModel.js
@@ -3,8 +3,8 @@ import format from 'pg-format';
 
 const Qr = {}
 
-Qr.insert = async(id, image,date,email) => {
-    return await pool.query(`INSERT INTO qr_codes VALUES ($1, $2, $3, $4)`, [id,image,date,email]);
+Qr.insert = async(id, image,date,email,color,url,title) => {
+    return await pool.query(`INSERT INTO qr_codes(id_qr, qr_image, time_qr_created, email, color, url, title) VALUES ($1, $2, $3, $4, $5, $6, $7)`, [id,image,date,email,color,url,title]);
 } 
 Qr.show = async(id) => {
     return await pool.query(`SELECT qr_image FROM qr_codes WHERE id_qr = $1`, [id]);
