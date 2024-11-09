79a5c1ad13e1a43a715e13ca624ad131dc0fa99d
yuzid <yazid.fauzan9999@gmail.com>
2024-11-09 16:51:08 +0700

    tsese

--- path.js
+++ path.js
@@ -2,4 +2,4 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
+export const __dirname = path.join("var/task", __dirname);
